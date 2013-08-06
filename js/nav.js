8147f61f8b71526e612ede09ad1d49d2af403dad
kelseygracebond <kgbond@sbcglobal.net>
2013-08-06 12:20:31 -0700

    responsive-basically done

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+<!--var $header = $('.header-wrapper');
+
+<!--$(window).scroll(function () {
+<!--   if(scrollY <= 0){
+<!--       $header.animate({
+<!--            height: 100
+<!--       }, 1000);
+<!--   }
+<!--   if(scrollY > 0 && $header.is(':not(:animated)')){
+<!--       $header.animate({
+<!--            height: 50
+<!--       }, 1000);
+<!--    }
+<!-- });
